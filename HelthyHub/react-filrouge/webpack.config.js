9874895b01f51531aac0b949af858bd3d9674101
Thiziri Bouaziz <ttalya2021@gmail.com>
2025-09-26 15:01:31 +0200

    update var

--- HelthyHub/react-filrouge/webpack.config.js
+++ HelthyHub/react-filrouge/webpack.config.js
@@ -1,12 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "src/[name].[fullhash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'src/[name].[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // ✅ important pour React Router
   },
   module: {
     rules: [
@@ -14,53 +17,64 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+            presets: ['@babel/preset-env', '@babel/preset-react'], // ✅ React JSX
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
           },
         },
       },
       {
         test: /\.(c|sa|sc)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
-      { test: /\.ts$/, use: "ts-loader" },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
     ],
   },
 
   devServer: {
-    historyApiFallback: true,
-    host: "127.0.0.1",
+    historyApiFallback: true, // ✅ routes React OK en dev
+    host: '127.0.0.1',
     port: 9090,
     open: true,
     hot: true,
     client: {
-      logging: "info",
+      logging: 'info',
       overlay: true,
       progress: true,
-      webSocketTransport: "ws",
+      webSocketTransport: 'ws',
     },
-    webSocketServer: "ws",
+    webSocketServer: 'ws',
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-      inject: "body",
+      template: path.resolve(__dirname, 'src/index.html'),
+      inject: 'body',
       hash: true,
     }),
     new ESLintPlugin({
-      extensions: "js",
-      exclude: "node_modules",
-      files: "./src/",
+      extensions: 'js',
+      exclude: 'node_modules',
+      files: './src/',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: '_redirects', to: '' }], // ✅ copie dans dist/
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
     }),
   ],
 };
+tgc-dkqz-hgb
